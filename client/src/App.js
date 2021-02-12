1b520e13fc34b953a0bde639c85b05b6452af157
robinsonwilbon <robinsonwilbon75@gmail.com>
2021-02-12 15:20:31 -0800

    Express server setup for initial routes

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import './App.css';
+
+const App = () => {
+    return (
+        <Router>
+            <div className='App'>
+            <Navbar />
+            My App
+            </div>
+        </Router>
+    );
+};
+
+
+export default App;
